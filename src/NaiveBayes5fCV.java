a0b500cc5ccbfbec5002a253ec7d2d22047d68f2
Radioactyve <radioactivex55@gmail.com>
2024-01-29 23:04:28 +0100

    3.bertsioa

--- src/NaiveBayes5fCV.java
+++ src/NaiveBayes5fCV.java
@@ -22,19 +22,27 @@ public class NaiveBayes5fCV {
         // String outPath = args[1];
         String outPath = "C:/Users/radio/Desktop/emaitzakArik2.txt";
 
-        //Integer folds= args[3]
-        Integer folds=5;
+
 
         // Klasea esleitu
         data.setClassIndex(data.numAttributes() - 1);
         // Atributuak aukeratu filtroa aplikatuz
-        AttributeSelection filter = applyAttributeSelection(data);
+        AttributeSelection filter = applyAttributeSelection(data); //(data) hartu jakiteko nolakoa izango den filtroa
         // Datuei filtroa aplikatu
         Instances newData = Filter.useFilter(data, filter);
         // NaiveBayes classifier-a adierazi
         NaiveBayes classifier = new NaiveBayes();
+
+        //METODOAK
         // 5-fold cross-validation
+        //Integer folds= args[3]
+        Integer folds=5;
         Evaluation evaluator = crossValidateModel(classifier, newData, folds, new Random(1));
+        // Hold-out
+        Integer percent=60;
+        Evaluation holdOutEvaluator = holdOutModel(classifier, newData, percent);
+
+
         // Emaitzak gorde
         saveResults(evaluator, outPath);
     }
@@ -55,21 +63,45 @@ public class NaiveBayes5fCV {
         evaluator.crossValidateModel(classifier, data, folds, seed);
         return evaluator;
     }
+    private static Evaluation holdOutModel(NaiveBayes classifier, Instances data, double percentage) throws Exception {
+        int numInstances = data.numInstances();
+
+        //trainInstances lortu
+        int numTrainInstances = (int) Math.round(numInstances * percentage / 100.0); //math round() zenbaki osoa lortzeko
+
+        //train eta test instatziak sortu
+        Instances trainData = new Instances(data, 0, numTrainInstances);
+        Instances testData = new Instances(data, numTrainInstances, numInstances - numTrainInstances);
+
+        //ebaluatu
+        classifier.buildClassifier(trainData);
+        Evaluation evaluator = new Evaluation(trainData);
+        evaluator.evaluateModel(classifier, testData);
+
+        return evaluator;
+    }
+
 
     private static void saveResults(Evaluation evaluator, String outputPath) throws Exception {
         FileWriter fileWriter = new FileWriter(outputPath);
-
         //Exekuzio data/ordua
         fileWriter.write("Data/ordua: " + java.time.LocalDateTime.now() + "\n\n");
+
         //Emaitza datuak
         fileWriter.write("Correctly Classified Instances: " + evaluator.pctCorrect() + " %\n");
         fileWriter.write("Incorrectly Classified Instances: " + evaluator.pctIncorrect() + " %\n" );
-        fileWriter.write("Kappa: " + evaluator.kappa() + " %\n\n\n" );
+        fileWriter.write("Kappa: " + evaluator.kappa() + " %\n" );
+        fileWriter.write("MAE: " + evaluator.meanAbsoluteError() + " %\n" );
+        fileWriter.write("RMSE: " + evaluator.rootMeanSquaredError() + " %\n" );
+        fileWriter.write("RAE: " + evaluator.relativeAbsoluteError() + " %\n" );
+        fileWriter.write("RRSE: " + evaluator.rootRelativeSquaredError() + " %\n\n\n" );
+
         //Nahasmen matrizea
         fileWriter.write(evaluator.toMatrixString());
+        fileWriter.write("\n\n" );
         //Precision metrikak
         fileWriter.write(evaluator.toClassDetailsString());
-        fileWriter.write("\nWeighted Avg Precision:\n" + evaluator.weightedPrecision());
+        fileWriter.write("\nWeighted Avg Precision:" + evaluator.weightedPrecision());
         //fileWriter Itxi
         fileWriter.flush();
         fileWriter.close();
