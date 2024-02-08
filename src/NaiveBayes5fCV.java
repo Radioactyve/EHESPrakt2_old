1299661fffc19dfae273d9f1cd84b8361ba920fc
Radioactyve <radioactivex55@gmail.com>
2024-02-08 21:02:32 +0100

    new v2 version

--- src/NaiveBayes5fCV.java
+++ src/NaiveBayes5fCV.java
@@ -1,77 +1,85 @@
-import weka.attributeSelection.BestFirst;
-import weka.attributeSelection.CfsSubsetEval;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.Instances;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
-import weka.core.Instances;
-import weka.core.converters.ConverterUtils;
+
+import weka.attributeSelection.BestFirst;
+import weka.attributeSelection.CfsSubsetEval;
+
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
+import weka.filters.supervised.instance.StratifiedRemoveFolds;
 
-import java.io.*;
 import java.util.Random;
+import java.io.*;
+
 
 public class NaiveBayes5fCV {
 
     public static void main(String[] args) throws Exception {
         // Datuak kargatu
         String inPath = args[0];
-        //String inPath = "C:/Users/radio/Desktop/System/Uni/3/Erabakiak/1. Praktika Datuak-20240122/heart-c.arff";
-        ConverterUtils.DataSource source = new ConverterUtils.DataSource(inPath);
+        DataSource source = new DataSource(inPath);
         Instances data = source.getDataSet();
         // Emaitzak gorde
         String outPath = args[1];
-        //String outPath = "C:/Users/radio/Desktop/emaitzakArik2.txt";
 
-
-        // Klasea esleitu
+        // Klasea adierazi
         data.setClassIndex(data.numAttributes() - 1);
         // Atributuak aukeratu filtroa aplikatuz
-        AttributeSelection filter = applyAttributeSelection(data); //(data) hartu jakiteko nolakoa izango den filtroa
+        AttributeSelection filter = new AttributeSelection();
+        filter.setEvaluator(new CfsSubsetEval());
+        filter.setSearch(new BestFirst());
+        filter.setInputFormat(data);
+
         // Datuei filtroa aplikatu
         Instances newData = Filter.useFilter(data, filter);
         // NaiveBayes classifier-a adierazi
         NaiveBayes classifier = new NaiveBayes();
 
-        //METODOAK
+        // ----------------------------------- [METODOAK] ---------------------------------------
         // 5-fold cross-validation
-        //Integer folds= args[3]
-        Integer folds=5;
-        Evaluation evaluator = crossValidateModel(classifier, newData, folds, new Random(1));
+        if (args[2].equals("1")){
+            Evaluation kfcv = new Evaluation(data);
+            kfcv.crossValidateModel(classifier, newData, 5, new Random(1));
+            saveResults(kfcv, outPath);
+        }
         // Hold-out
-        Integer percent=60;
-        Evaluation holdOutEvaluator = holdOutModel(classifier, newData, percent);
+        else if (args[2].equals("2")){
+            Evaluation holdOut = holdOutModel(classifier, newData);
+            saveResults(holdOut, outPath);
+        }
+        // Stratified Hold-out
+        else{
+            // Estratificar el conjunto de datos
+            StratifiedRemoveFolds stratifiedFilter = new StratifiedRemoveFolds();
+            stratifiedFilter.setNumFolds(2); // Dividir en dos conjuntos
+            stratifiedFilter.setFold(1); // Seleccionar el primer conjunto (entrenamiento)
+            stratifiedFilter.setInputFormat(newData);
+            Instances trainData = Filter.useFilter(newData, stratifiedFilter);
 
+            // Obtener el conjunto de datos restante (pruebas)
+            stratifiedFilter.setFold(2); // Seleccionar el segundo conjunto (pruebas)
+            Instances testData = Filter.useFilter(newData, stratifiedFilter);
 
-        // Emaitzak gorde
-        saveResults(evaluator, outPath);
+            // Entrenar el clasificador
+            classifier.buildClassifier(trainData);
+
+            // Evaluar el clasificador en el conjunto de pruebas
+            Evaluation sHoldOut = new Evaluation(trainData);
+            sHoldOut.evaluateModel(classifier, testData);
+
+            // Guardar los resultados en el archivo de salida
+            saveResults(sHoldOut, outPath);
+        }
     }
 
-    private static AttributeSelection applyAttributeSelection(Instances data) throws Exception {
-        AttributeSelection filter = new AttributeSelection();
-        CfsSubsetEval eval = new CfsSubsetEval();
-        BestFirst search = new BestFirst();
-        filter.setEvaluator(eval);
-        filter.setSearch(search);
-        filter.setInputFormat(data);
-        return filter;
-    }
-
-    private static Evaluation crossValidateModel(NaiveBayes classifier, Instances data, int folds, Random seed)
-            throws Exception {
-        Evaluation evaluator = new Evaluation(data);
-        evaluator.crossValidateModel(classifier, data, folds, seed);
-        return evaluator;
-    }
-    private static Evaluation holdOutModel(NaiveBayes classifier, Instances data, double percentage) throws Exception {
-        int numInstances = data.numInstances();
-
-        //trainInstances lortu
-        int numTrainInstances = (int) Math.round(numInstances * percentage / 100.0); //math round() zenbaki osoa lortzeko
-
+    private static Evaluation holdOutModel(NaiveBayes classifier, Instances data) throws Exception {
         //train eta test instatziak sortu
+        int numInstances = data.numInstances();
+        int numTrainInstances = (int) Math.round(numInstances * 0.66); //math round() zenbaki osoa lortzeko
         Instances trainData = new Instances(data, 0, numTrainInstances);
         Instances testData = new Instances(data, numTrainInstances, numInstances - numTrainInstances);
-
         //ebaluatu
         classifier.buildClassifier(trainData);
         Evaluation evaluator = new Evaluation(trainData);
@@ -96,12 +104,12 @@ public class NaiveBayes5fCV {
         fileWriter.write("RRSE: " + evaluator.rootRelativeSquaredError() + " %\n\n\n" );
 
         //Nahasmen matrizea
-        fileWriter.write(evaluator.toMatrixString());
-        fileWriter.write("\n\n" );
+        fileWriter.write(evaluator.toMatrixString() + "\n\n");
         //Precision metrikak
         fileWriter.write(evaluator.toClassDetailsString());
         fileWriter.write("\nWeighted Avg Precision:" + evaluator.weightedPrecision());
-        //fileWriter Itxi
+
+        //fileWriter itxi
         fileWriter.flush();
         fileWriter.close();
     }
